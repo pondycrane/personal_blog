775308269cddc37d9af57c7755c7d76441e12efa
Hank Huang <pondycrane@gmail.com>
2018-01-06 15:01:18 -0500

    solve mongoose issue

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ import serverConfig from './config';
 mongoose.Promise = global.Promise;
 
 // MongoDB Connection
-mongoose.connect(serverConfig.mongoURL, (error) => {
+mongoose.connect(serverConfig.mongoURL, { useMongoClient: true }, (error) => {
   if (error) {
     console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
     throw error;
